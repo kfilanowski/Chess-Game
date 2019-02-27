2b39e0300403b264dd3cc68ccc85b47054fb4599
Lutzmatt17 <Lutzmatt17@gmail.com>
2019-02-26 22:07:08 -0500

    Testing from my desktop

--- src/test.java
+++ src/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String[] args){
-        System.out.println("Testing, Testing, Testing....");
+        System.out.println("Testing, testing, 1, 2, 3...");
     }
 }
